cba216f240bb424bb0842f36a0232814e1e82f86
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-09 15:57:37 +0800

    [Tracy] practice_5

--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,1 +1,11 @@
+import Person from "../../src/practice_5/person.js";
 
+export default class Worker extends Person{
+    constructor(name,age){
+        super(name,age);
+    }
+
+    introduce(){
+        return `${super.introduce()} I am a Teacher. I have a job.`;
+    }
+}
