ff1f2b42fed5da31ed86adeb74507b6a88ccaff0
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-09 20:54:43 +0800

    [Tracy] practice_9

--- src/practice_9/class.js
+++ src/practice_9/class.js
@@ -1,1 +1,24 @@
+export default class Class{
+    constructor(number){
+        this.number = number;
+    }
+    
+    leader;
 
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student){
+        if(student.klass.number === this.number)
+            this.leader = student;
+        else {
+                this.leader = undefined;
+                console.log("It is not one of us.");
+        }
+    }
+
+    appendMember(student) {
+        student.klass = this;
+    }
+}
