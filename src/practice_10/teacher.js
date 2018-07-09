f49e297467e13a3a2346e263bd0809762fc872fb
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-09 22:16:40 +0800

    [Tracy] practice_10

--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,24 @@
+import Person from "../../src/practice_8/person.js";
 
+export default class Teacher extends Person{
+    constructor(id,name,age,klasses){
+        super(id,name,age);
+        this.klasses = klasses;
+    }
+
+    introduce(){
+        if(this.klasses === undefined || this.klasses.length === 0) 
+            return `${super.introduce()} I am a Teacher. I teach No Class.`;
+        else 
+            return `${super.introduce()} I am a Teacher. I teach Class ${this.klasses.map(x => x.number).join(", ")}.`;
+    }
+
+    introduceWith(student){
+        let isStudent = this.klasses.some( klass => klass === student.klass);
+        if(isStudent){
+            return `${super.introduce()} I am a Teacher. I teach ${student.name}.`;
+        }else{
+            return `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`;
+        }
+}
+}
