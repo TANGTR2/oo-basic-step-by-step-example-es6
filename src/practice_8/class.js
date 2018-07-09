a75a983a99c6bd6c4dc4d4639de4e2de96c87bdb
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-09 20:20:25 +0800

    [Tracy] practice_8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,17 @@
+export default class Class{
+    constructor(number){
+        this.number = number;
+    }
+    
+    leader;
 
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student){
+        if(student.klass.number === this.number)
+            this.leader = student;
+        else this.leader = undefined;
+    }
+}
--- src/practice_8/student.js
+++ src/practice_8/student.js
@@ -1,1 +1,20 @@
+import Person from "../../src/practice_8/person.js";
 
+export default class Student extends Person{
+    constructor(id,name,age,klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+
+    introduce(){
+        if(this.klass.leader != undefined){
+            if(this.klass.leader.id === this.id)
+                return `${super.introduce()} I am a Student. I am Leader of Class ${this.klass.number}.`;
+            else
+                return `${super.introduce()} I am a Student. I am at Class ${this.klass.number}.`;
+        }
+        else
+            return `${super.introduce()} I am a Student. I am at Class ${this.klass.number}.`;
+        
+    }
+}
