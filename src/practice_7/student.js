f73a24e470e2adaaa9b235991463701a144ecd77
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-09 17:04:01 +0800

    [Tracy] practice_7

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,12 @@
+import Person from "../../src/practice_7/person.js";
 
+export default class Student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    introduce(){
+        return `${super.introduce()} I am a Student. I am at Class ${this.klass.number}.`;
+    }
+}
